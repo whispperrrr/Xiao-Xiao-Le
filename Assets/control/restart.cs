efc2a607a163b42c90b70fad2ae6e5f42090c8be
huangyuxuan77 <2039890161@qq.com>
2024-07-13 01:24:45 +0800

    Add files via upload

---
+++ Assets/control/restart.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class SceneLoader : MonoBehaviour
+{
+   
+    //使用场景名称来加载  
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
